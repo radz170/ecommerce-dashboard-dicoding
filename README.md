163548e16f7cc800f4790937e23f8421b163c443
radz170 <riadinriadin1@gmail.com>
2026-03-11 15:26:47 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# E-Commerce Public Data Analysis Dashboard
+
+## Deskripsi Proyek
+Proyek ini merupakan submission untuk kelas **Belajar Analisis Data dengan Python** di Dicoding. Dashboard ini dibuat menggunakan **Streamlit** untuk memvisualisasikan hasil eksplorasi dan analisis data dari *E-Commerce Public Dataset*.
+
+Dashboard ini terdiri dari 5 bagian utama yang menjawab berbagai pertanyaan bisnis:
+1. **Daily Orders:** Menampilkan tren metrik jumlah pesanan harian dan total pendapatan (*revenue*).
+2. **Customer Demographics:** Menampilkan persebaran pelanggan berdasarkan Negara Bagian (*State*) dan Kota (*City*) khusus untuk pesanan yang berhasil terkirim.
+3. **Delivery Time Analysis:** Menganalisis rata-rata waktu pengiriman pesanan dan distribusi keterlambatan pengiriman.
+4. **Best Performing Product Categories:** Mengidentifikasi kategori produk paling laris berdasarkan volume penjualan (jumlah item) dan total pendapatan.
+5. **RFM Analysis:** Menyajikan segmentasi pelanggan terbaik berdasarkan parameter *Recency* (Keterbaruan), *Frequency* (Frekuensi), dan *Monetary* (Nilai Uang).
+
+## Struktur Direktori
+- `/data`: Berisi dataset mentah berformat `.csv` yang digunakan dalam proyek ini.
+- `/dashboard`: Berisi file utama `dashboard.py` dan dataset bersih `all_data.csv` yang digunakan untuk aplikasi Streamlit.
+- `Proyek_Analisis_Data.ipynb`: File Jupyter Notebook yang berisi alur analisis data lengkap dari tahap *Data Wrangling* hingga *Explanatory Analysis*.
+- `README.md`: Dokumentasi petunjuk informasi dan penggunaan proyek.
+- `requirements.txt`: Daftar *library* Python yang dibutuhkan untuk menjalankan proyek ini.
+- `url.txt`: Berisi tautan (URL) untuk mengakses dashboard interaktif yang sudah di-deploy ke Streamlit Cloud.
+
+## Setup Environment
+**Menggunakan Anaconda:**
+```bash
+conda create --name main-ds python=3.9
+conda activate main-ds
+pip install -r requirements.txt
---
+++ dashboard.py
@@ -0,0 +1,238 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+
+sns.set(style='dark')
+
+#Sebelumnya saya menyiapkan DataFrame yang akan digunakan untuk membuat visualisasi data.
+#Untuk melakukan hal ini, saya perlu membuat beberapa helper function.
+
+#1. create_daily_orders_df digunakan untuk menghitung total pesanan dan total pendapatan (revenue) harian.
+def create_daily_orders_df(df):
+    daily_orders_df = df.resample(rule='D', on='order_purchase_timestamp').agg({
+        "order_id": "nunique",
+        "price": "sum"
+    })
+    daily_orders_df = daily_orders_df.reset_index()
+    daily_orders_df.rename(columns={
+        "order_id": "order_count",
+        "price": "revenue"
+    }, inplace=True)
+    return daily_orders_df
+
+#2. create_bycity_df untuk menyiapkan data pelanggan terbanyak berdasarkan kota (city)(Menjawab Pertanyaan 1: Demografi Kota).
+def create_bycity_df(df):
+    bycity_df = df.groupby(by="customer_city").customer_id.nunique().reset_index()
+    bycity_df.rename(columns={
+        "customer_id": "customer_count"
+    }, inplace=True)
+    
+    bycity_df = bycity_df.sort_values(by="customer_count", ascending=False)
+    return bycity_df
+
+#3. create_bystate_df melihat negara bagian (state) mana yang memiliki pelanggan terbanyak. (Menjawab Pertanyaan 1: Demografi State)
+def create_bystate_df(df):
+    bystate_df = df.groupby(by="customer_state").customer_id.nunique().reset_index()
+    bystate_df.rename(columns={
+        "customer_id": "customer_count"
+    }, inplace=True)
+    
+    # Mengurutkan dari yang terbanyak
+    bystate_df = bystate_df.sort_values(by="customer_count", ascending=False)
+    return bystate_df
+
+#4. create_delivery_time_df untuk menampilkan waktu pengiriman (menjawab Pertanyaan 2 (Waktu Pengiriman))
+def create_delivery_time_df(df):
+    # Memastikan tidak ada nilai kosong pada kolom tanggal pengiriman
+    delivery_df = df.dropna(subset=['order_purchase_timestamp', 'order_delivered_customer_date']).copy()
+    
+    # Hapus duplikat order_id agar pesanan dengan banyak item tidak dihitung berulang kali
+    delivery_df = delivery_df.drop_duplicates(subset=['order_id'])
+    
+    # Menghitung selisih waktu persis seperti di ipynb
+    delivery_time = delivery_df["order_delivered_customer_date"] - delivery_df["order_purchase_timestamp"]
+    delivery_time = delivery_time.apply(lambda x: x.total_seconds())
+    delivery_df["delivery_time_days"] = round(delivery_time/86400)
+    
+    return delivery_df
+
+#5. create_sum_order_items_df untuk membuat grafik batang kategori produk yang paling banyak terjual (volume) dan 
+# paling banyak menghasilkan uang (revenue).(Menjawab Pertanyaan 3: Produk Laris)
+def create_sum_order_items_df(df):
+    sum_order_items_df = df.groupby("product_category_name").agg({
+        "order_id": "count",  # Menghitung jumlah barang terjual
+        "price": "sum"        # Menghitung total pendapatan
+    }).reset_index()
+    
+    # Mengurutkan berdasarkan pendapatan terbesar
+    sum_order_items_df = sum_order_items_df.sort_values(by="price", ascending=False)
+    return sum_order_items_df
+
+#6. create_rfm_df untuk menjalankan logika RFM (Untuk Analisis Lanjutan: RFM)
+def create_rfm_df(df):
+    rfm_df = df.groupby(by="customer_unique_id", as_index=False).agg({
+        "order_purchase_timestamp": "max", # Mengambil tanggal order terakhir
+        "order_id": "nunique",             # Menghitung jumlah order unik
+        "price": "sum"                     # Menghitung jumlah revenue
+    })
+    rfm_df.columns = ["customer_id", "max_order_timestamp", "frequency", "monetary"]
+    
+    # Menghitung Recency (hari)
+    rfm_df["max_order_timestamp"] = rfm_df["max_order_timestamp"].dt.date
+    recent_date = df["order_purchase_timestamp"].dt.date.max()
+    rfm_df["recency"] = rfm_df["max_order_timestamp"].apply(lambda x: (recent_date - x).days)
+    
+    rfm_df.drop("max_order_timestamp", axis=1, inplace=True)
+    return rfm_df
+
+#load berkas all_data.csv sebagai sebuah DataFrame
+all_df = pd.read_csv("all_data.csv")
+
+# Mengubah tipe data menjadi datetime
+datetime_columns = ["order_purchase_timestamp", "order_delivered_customer_date"]
+all_df.sort_values(by="order_purchase_timestamp", inplace=True)
+all_df.reset_index(inplace=True)
+
+for column in datetime_columns:
+    all_df[column] = pd.to_datetime(all_df[column])
+
+# Membuat Filter Sidebar
+min_date = all_df["order_purchase_timestamp"].min()
+max_date = all_df["order_purchase_timestamp"].max()
+
+with st.sidebar:
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+    
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',
+        min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
+# Filter data utama
+main_df = all_df[(all_df["order_purchase_timestamp"].dt.date >= start_date) & 
+                 (all_df["order_purchase_timestamp"].dt.date <= end_date)]
+
+# Panggil Helper Functions
+daily_orders_df = create_daily_orders_df(main_df)
+bycity_df = create_bycity_df(main_df)
+bystate_df = create_bystate_df(main_df)
+delivery_time_df = create_delivery_time_df(main_df)
+sum_order_items_df = create_sum_order_items_df(main_df)
+rfm_df = create_rfm_df(main_df)
+
+#MELENGKAPI DASHBOARD DENGAN VISUALISASI DATA
+#Buat header dulu
+st.header('E-Commerce Public Dashboard')
+
+# 1. Visualisasi Data Daily Orders
+st.subheader('Daily Orders')
+col1, col2 = st.columns(2)
+with col1:
+    total_orders = daily_orders_df.order_count.sum()
+    st.metric("Total orders", value=total_orders)
+with col2:
+    total_revenue = format_currency(daily_orders_df.revenue.sum(), "USD", locale='es_CO') 
+    st.metric("Total Revenue", value=total_revenue)
+
+# Grafik Garis untuk Daily Orders
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(daily_orders_df["order_purchase_timestamp"], daily_orders_df["order_count"], marker='o', linewidth=2, color="#90CAF9")
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+st.pyplot(fig)
+
+# 2. Visualisasi Data Pertanyaan 1: Demografi
+st.subheader("Customer Demographics")
+col1, col2 = st.columns(2)
+with col1:
+    fig, ax = plt.subplots(figsize=(20, 10))
+    sns.barplot(x="customer_count", y="customer_state", data=bystate_df.head(5), palette="Blues_d", ax=ax)
+    ax.set_title("Number of Customer by States", loc="center", fontsize=30)
+    ax.tick_params(axis='y', labelsize=20)
+    ax.tick_params(axis='x', labelsize=25)
+    st.pyplot(fig)
+with col2:
+    fig, ax = plt.subplots(figsize=(20, 10))
+    sns.barplot(x="customer_count", y="customer_city", data=bycity_df.head(5), palette="Blues_d", ax=ax)
+    ax.set_title("Number of Customer by City", loc="center", fontsize=30)
+    ax.tick_params(axis='y', labelsize=20)
+    ax.tick_params(axis='x', labelsize=25)
+    st.pyplot(fig)
+
+st.caption("Catatan: Demografi dihitung berdasarkan pelanggan dengan pesanan yang berhasil terkirim (Delivered).")
+
+# 3. Visualisasi Data Pertanyaan 2: Delivery Time
+st.subheader("Delivery Time Analysis")
+col1, col2 = st.columns(2)
+with col1:
+    st.metric("Avg Delivery Time (Days)", value=round(delivery_time_df['delivery_time_days'].mean(), 1))
+with col2:
+    st.metric("Max Delivery Delay (Days)", value=delivery_time_df['delivery_time_days'].max())
+
+fig, ax = plt.subplots(figsize=(16, 8))
+sns.histplot(x='delivery_time_days', data=delivery_time_df, bins=50, color="#90CAF9", kde=True, ax=ax)
+ax.set_title("Distribution of Delivery Time", fontsize=20)
+st.pyplot(fig)
+
+# Visualisasi Data Pertanyaan 3: Produk Terlaris
+st.subheader("Best Performing Product Categories")
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+# By Volume
+sns.barplot(x="order_id", y="product_category_name", data=sum_order_items_df.sort_values(by="order_id", ascending=False).head(5), palette=colors, ax=ax[0])
+ax[0].set_title("By Volume (Items Sold)", loc="center", fontsize=50)
+ax[0].tick_params(axis='y', labelsize=35)
+ax[0].tick_params(axis='x', labelsize=30)
+
+# By Revenue
+sns.barplot(x="price", y="product_category_name", data=sum_order_items_df.head(5), palette=colors, ax=ax[1])
+ax[1].set_title("By Revenue ($)", loc="center", fontsize=50)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+st.pyplot(fig)
+
+# 5. Visualisasi Data Analisis Lanjutan: RFM
+st.subheader("Best Customer Based on RFM Parameters")
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric("Average Recency (days)", value=round(rfm_df.recency.mean(), 1))
+with col2:
+    st.metric("Average Frequency", value=round(rfm_df.frequency.mean(), 2))
+with col3:
+    st.metric("Average Monetary", value=format_currency(rfm_df.monetary.mean(), "USD", locale='es_CO'))
+
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(35, 15))
+colors = ["#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9"]
+
+# Recency
+top_recency = rfm_df.sort_values(by="recency", ascending=True).head(5).copy()
+top_recency['short_id'] = top_recency['customer_id'].str[:5]
+sns.barplot(y="recency", x="short_id", data=top_recency, palette=colors, ax=ax[0])
+ax[0].set_title("By Recency (days)", loc="center", fontsize=50)
+ax[0].tick_params(axis='x', labelsize=35)
+
+# Frequency
+top_freq = rfm_df.sort_values(by="frequency", ascending=False).head(5).copy()
+top_freq['short_id'] = top_freq['customer_id'].str[:5]
+sns.barplot(y="frequency", x="short_id", data=top_freq, palette=colors, ax=ax[1])
+ax[1].set_title("By Frequency", loc="center", fontsize=50)
+ax[1].tick_params(axis='x', labelsize=35)
+
+# Monetary
+top_monetary = rfm_df.sort_values(by="monetary", ascending=False).head(5).copy()
+top_monetary['short_id'] = top_monetary['customer_id'].str[:5]
+sns.barplot(y="monetary", x="short_id", data=top_monetary, palette=colors, ax=ax[2])
+ax[2].set_title("By Monetary", loc="center", fontsize=50)
+ax[2].tick_params(axis='x', labelsize=35)
+
+st.pyplot(fig)
+
+st.caption('Copyright (c) Dicoding Submission 2026')
